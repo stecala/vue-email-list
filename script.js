e18817d67ef7c139d2e7874c6a71f4b831fe5a55
stecala <ste.cala123@gmail.com>
2022-07-13 15:24:17 +0200

    featured ⭐ function that populate the array

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const app = new Vue({
-    el : '#app',
-    data : {
-        
+    el: '#app',
+    data: {
+        arrayMail: []
     },
-    method : {
-    
+    method: {
+        callBackApi(array) {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((element) => {
+                        array.push(element.response)
+                    })
+            }
+        },
     },
-    created : {
-    
-    },
+    created() {
+            this.callBackApi(this.arrayMail)
+        }
 })
